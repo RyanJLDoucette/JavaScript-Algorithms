b4839130f2b80e711aa17887de2a5523f8f908c6
Ryan Doucette <31770773+RyanJLDoucette@users.noreply.github.com>
2018-08-20 19:29:21 -0300

    Started the algorithm

---
+++ returningchange.js
@@ -0,0 +1,10 @@
+/*Problem: Write a JavaScript function that takes an amount in cents, and tells us which coins, 
+and how many of them we need to make up that amount.
+The coins available are 100,25, 10, 5 or 1 cents.*/
+
+/*takes in an amount of money (eg: 1.56 represents 1 dollar and 56 cents) and returns an array of what coins would be returned
+Sample output: makechange(1.16) returns -> [1,10,5,1]*/
+function makeChange (money) {
+
+}
+
